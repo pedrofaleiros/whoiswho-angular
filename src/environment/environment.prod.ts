e6b45321fd8bc40fdd41a98efb6a1ab007ecfbec
pedrofaleiros <pedrocrfaleiros@yahoo.com.br>
2025-09-01 17:39:00 -0300

    commit

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: '${API_URL}',
-    SOCKET_URL: '${SOCKET_URL}'
+    API_URL: "https://whoiswho-api-909640324508.southamerica-east1.run.app",
+    SOCKET_URL: "https://whoiswho-ws-909640324508.southamerica-east1.run.app",
 }
--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     // API_URL: "http://192.168.0.129:8080",
     // SOCKET_URL: "http://192.168.0.129:3000",
-    SOCKET_URL: "https://backend-node-909640324508.southamerica-east1.run.app",
-    API_URL: "https://backend-spring-909640324508.us-east1.run.app"
+    API_URL: "https://whoiswho-api-909640324508.southamerica-east1.run.app",
+    SOCKET_URL: "https://whoiswho-ws-909640324508.southamerica-east1.run.app",
 }
